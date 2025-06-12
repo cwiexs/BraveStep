e5e0987c16c9146513dcaddf23172397562d3dda
cwiexs <cwiexs@gmail.com>
2025-06-12 21:43:53 +0100

    Delete loading.tsx

--- app/(sport)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from '@/components/LoadingSpinner';
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <LoadingSpinner />
-    </div>
-  );
-}
