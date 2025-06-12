6bfb8a0e378db207b3f77345430acc41409c0394
cwiexs <cwiexs@gmail.com>
2025-06-12 23:41:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import PlanForm from '@/components/PlanForm';
-
 export default function Home() {
-  return (
-    <main className="container mx-auto py-12">
-      <h1 className="text-3xl font-bold mb-6 text-center">AI Sporto Planai</h1>
-      <PlanForm />
-    </main>
-  );
+  return <h1>Hello world</h1>;
 }
