4c658de8249348b4c17442998bf4a3cac878bf1f
cwiexs <cwiexs@gmail.com>
2025-06-12 23:41:32 +0100

    Delete PlanResult.tsx

--- components/PlanResult.tsx
+++
@@ -1,7 +0,0 @@
-export default function PlanResult({ plan }: { plan: string }) {
-  return (
-    <section className="mt-8 whitespace-pre-wrap p-4 border rounded bg-white shadow">
-      {plan}
-    </section>
-  );
-}
