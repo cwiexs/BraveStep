9614cdb375f61410d55b90e7dd2e079e749ce3eb
cwiexs <cwiexs@gmail.com>
2025-06-13 22:05:06 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,33 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-export default NextAuth({
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  // Čia gali pridėti papildomus nustatymus jei norėsi ateityje (pvz., callbacks, pages ir t.t.)
-});
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/"
+  }
+};
+
+export default NextAuth(authOptions);
