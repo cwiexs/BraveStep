0cf969c59a9c2e3dfb4ec77415054fc036531e4d
cwiexs <cwiexs@gmail.com>
2025-06-13 20:37:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
-  return <h1>BraveStep veikia!</h1>
+  // Saugus variantas: priskirk numatytą reikšmę, jei useSession() grąžina undefined
+  const sessionData = useSession?.();
+  const session = sessionData?.data;
+
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 32 }}>
+      <h1>BraveStep</h1>
+      {session ? (
+        <>
+          <p>Prisijungęs kaip {session.user.email}</p>
+          <button onClick={() => signOut()}>Atsijungti</button>
+          <a href="/dashboard"><button>Mano planai</button></a>
+        </>
+      ) : (
+        <>
+          <button onClick={() => signIn("google")}>Prisijungti su Google</button>
+          <button onClick={() => signIn("facebook")}>Prisijungti su Facebook</button>
+          {/* <button onClick={() => signIn("email")}>Prisijungti el. paštu</button> */}
+        </>
+      )}
+    </main>
+  );
 }
