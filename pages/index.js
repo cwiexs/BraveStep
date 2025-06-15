4b76a05c393d13d74a5133f2496f1a86942e972e
cwiexs <cwiexs@gmail.com>
2025-06-15 20:50:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // pages/index.js
 import Head from 'next/head';
+import Link from 'next/link';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
@@ -16,12 +17,33 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>BraveStep Sporto Planai</title>
+        <title>BraveStep</title>
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
         <h1 className="text-5xl font-bold mb-8">BraveStep</h1>
 
-        {session ? (
+        {!session ? (
+          <div className="space-y-4 text-center">
+            <button
+              onClick={() => signIn()}
+              className="w-64 px-6 py-2 rounded bg-green-600 text-white hover:bg-green-700 transition"
+            >
+              Prisijungti (El. paštu)
+            </button>
+            <button
+              onClick={() => signIn('facebook')}
+              className="w-64 px-6 py-2 rounded bg-blue-700 text-white hover:bg-blue-800 transition"
+            >
+              Prisijungti per Facebook
+            </button>
+            <p>
+              Neturi paskyros?{' '}
+              <Link href="/auth/signup">
+                <a className="text-blue-600 hover:underline">Sukurti paskyrą</a>
+              </Link>
+            </p>
+          </div>
+        ) : (
           <div className="text-center">
             <p className="mb-4">Sveiki, {session.user.name}!</p>
             <button
@@ -31,13 +53,6 @@ export default function Home() {
               Atsijungti
             </button>
           </div>
-        ) : (
-          <button
-            onClick={() => signIn('facebook')}
-            className="px-6 py-2 rounded bg-blue-700 text-white hover:bg-blue-800 transition"
-          >
-            Prisijungti per Facebook
-          </button>
         )}
       </div>
     </>
