26d911f5a4a198693e647114895b1102ff775d19
cwiexs <cwiexs@gmail.com>
2025-06-23 17:19:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react';
 import { appWithTranslation } from 'next-i18next';
+import { SessionProvider } from 'next-auth/react';
 
-
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
+
 export default appWithTranslation(App);
