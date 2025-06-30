82fa324712370745b609a3f1dae2399c7b5e0bd1
cwiexs <cwiexs@gmail.com>
2025-06-30 12:07:52 +0100

    Update BookPageLayout.js

--- components/BookPageLayout.js
+++ components/BookPageLayout.js
@@ -2,9 +2,10 @@ import LanguageTab from './LanguageTab';
 
 export default function BookPageLayout({ children }) {
   return (
-    <div className="relative max-w-5xl mx-auto rounded-3xl shadow-lg bg-white p-6 md:p-12 my-8 min-h-[80vh] flex flex-col">
+    <div className="relative max-w-5xl mx-auto rounded-3xl shadow-lg bg-white p-6 md:p-12 mt-20 mb-8 min-h-[80vh] flex flex-col">
       <LanguageTab /> 
       {children}
     </div>
+    
   );
 }
