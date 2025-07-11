f0f007c077aadb358dcac1079569a97880f07f13
cwiexs <cwiexs@gmail.com>
2025-07-11 20:19:04 +0100

    a
    a

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -276,7 +276,7 @@ const sections = [
       {
         name: "healthConditions",
         label: "form.healthConditions",
-        type: "input",
+        type: "array",
         infoKey: "info.healthConditions",
       },
       {
