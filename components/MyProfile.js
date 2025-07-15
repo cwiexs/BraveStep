7e7ce4f6e549056c1c60abb9306b5da2ac9fd80b
cwiexs <cwiexs@gmail.com>
2025-07-15 22:00:33 +0100

    Update MyProfile.js

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import React, { useState, useEffect, useMemo } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
