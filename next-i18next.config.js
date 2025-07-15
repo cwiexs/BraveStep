b926cb0a7851aad516eb1edeb88222fccbf6ff56
cwiexs <cwiexs@gmail.com>
2025-07-15 19:23:04 +0100

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,6 +6,6 @@ module.exports = {
       ? require('path').resolve('./public/locales')
       : '/locales',
   },
-  ns: ['common', 'eatingHabitsTranslations'], // Pridėtas eatingHabits namespace
+  ns: ['common', 'eatingHabits'], // Pridėtas eatingHabits namespace
   defaultNS: 'common', // Nustatomas default namespace
 };
