d574537172c031b8ab33d51785fdab9de136f251
cwiexs <cwiexs@gmail.com>
2025-07-23 23:28:43 +0100

    Update WorkoutPlayer.js

--- components/WorkoutPlayer.js
+++ components/WorkoutPlayer.js
@@ -164,7 +164,7 @@ export default function WorkoutPlayer({ workoutData, onClose }) {
               className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg font-semibold"
               onClick={handleManualContinue}
             >
-              Tęsti
+              Atlikta
             </button>
           </div>
         )}
