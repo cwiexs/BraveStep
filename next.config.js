61fdc90febfb1d2fd2a0f6a3e72d900af2ed11aa
cwiexs <cwiexs@gmail.com>
2025-09-11 12:17:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,19 @@
 // next.config.js
-// Debug jungiklis: NUSTATYK NEXT_DEBUG=1, kai reikia aiškių stack trace (be minifikavimo)
-const DEBUG = process.env.NEXT_DEBUG === '1';
+const DEBUG = true; // LAIKINAI true, tik diagnostikai
 
-// Importuojame i18n bazinę konfigūraciją (jei yra)
 const { i18n: i18nFull = {} } = require('./next-i18next.config');
-
-// Leiskime tik Next leidžiamus i18n laukus ir nustatykime patikimą EN fallback
 const i18n = {
   locales: i18nFull.locales || ['en', 'lt'],
-  defaultLocale: i18nFull.defaultLocale || 'en',   // <- numatytoji ANGŲ
+  defaultLocale: i18nFull.defaultLocale || 'en',
   localeDetection: i18nFull.localeDetection ?? true,
   ...(i18nFull.domains ? { domains: i18nFull.domains } : {}),
 };
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-
-  // Kai DEBUG=1: išjungiame minifikavimą ir įjungiame source maps net production'e
-  swcMinify: !DEBUG,
-  productionBrowserSourceMaps: DEBUG,
-
-  // Papildomai: aiškesni source maps ir devtool, kai DEBUG=1
-  webpack: (config, { dev, isServer }) => {
-    if (DEBUG) {
-      // išlaikome pilnus source maps naršyklėje
-      config.devtool = 'source-map';
-    }
-    return config;
-  },
-
-  // i18n pririšame čia
+  swcMinify: !DEBUG,                 // išjungiame minifikavimą
+  productionBrowserSourceMaps: DEBUG, // įjungiame source maps
+  webpack: (config) => { config.devtool = 'source-map'; return config; },
   i18n,
 };
-
-module.exports = nextConfig;
